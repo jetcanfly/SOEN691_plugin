2dd0dcd0719dfb16fe80f39ffc4d6bf2e38d4c30
Deanxx51 <35351330+Deanxx51@users.noreply.github.com>
2019-04-10 23:23:51 -0400

    Table-Metric-Try Scope-Condition

--- src/tutorial691/visitors/TrySizeVisitor.java
+++ src/tutorial691/visitors/TrySizeVisitor.java
@@ -18,17 +18,42 @@ import java.io.LineNumberReader;
 public class TrySizeVisitor extends ASTVisitor{
 	int tryQuantity = 0;
 	int tryLOC =0;
-
+	int tryCondition =0;
 
 
 	
 	public boolean visit(TryStatement node) {
 		this.tryQuantity++;
 		this.getLOC(node);
-
+		this.getConditionInTry(node);
 		return super.visit(node);
 	}
 
+	public  void getConditionInTry(TryStatement node) {
+		
+		getIfInTry(node);
+		getSwitchInTry(node);
+	}
+
+	public void getIfInTry(TryStatement node) {
+		String a = node.getBody().toString();
+		for(int i=0; i<a.length()-2;i++) {
+			String tempif = a.substring(0+i,3+i);
+			if(tempif.equals("if ")) {
+				tryCondition++;
+			}
+		}
+	}
+	
+	public void getSwitchInTry(TryStatement node) {
+		String a = node.getBody().toString();
+		for(int i=0; i<a.length()-6;i++) {
+			String tempSwitch = a.substring(0+i,7+i);
+			if(tempSwitch.equals("switch ")) {
+				tryCondition++;
+			}
+		}
+	}
 	public  void getLOC(TryStatement node) {
 		String a = node.getBody().toString();
 		for(int i=0; i<a.length();i++) {
@@ -38,6 +63,10 @@ public class TrySizeVisitor extends ASTVisitor{
 		}
 	}
 
+	public int getTryCondition() {
+		return tryCondition;
+	}
+	
 	public int getTrySize() {
 		return tryLOC;
 	}
